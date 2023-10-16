6af121a80f6c5bdf943924511b37bf3c7b8f9d81
KimHongHee <qaz101044@naver.com>
2023-10-16 20:48:08 +0900

    test_1

---
+++ src/main/java/com/example/ParkingLot_SEOUL/ParkingLotSeoulApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParkingLot_SEOUL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotSeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParkingLot_SEOUL/ParkingLotSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParkingLot_SEOUL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
